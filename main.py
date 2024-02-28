b682a33257a234ebfb4f529caa015cbe91c1eaca
Boris Mauricio Tapia <btapiaf@hotmail.com>
2024-02-28 12:21:31 -0500

    Tarea de multiplicacion resuelta

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 def sumar(a, b):
+    return a+b
     # aqui se realiza la suma de dos numeros
 
 def restar(a, b):
+    return a-b
     # aqui se realiza la resta de dos numeros
 
 def multiplicar(a, b):
     # aqui se realiza la multiplicacion de dos numeros
-
+    return a*b
 def dividir(a, b):
     # aqui se realiza la division de dos numeros
-
+    if(a==0 or b==0):
+        return "Error: No se puede dividir por cero"
+    return a/b
 def main():
     num1 = float(input("Ingrese el primer número: "))
     num2 = float(input("Ingrese el segundo número: "))
